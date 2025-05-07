329018c64abeb11cb13520bef177a2fc40ff26b8
Narx <ameereking@gmail.com>
2025-05-08 00:01:38 +0100

    retrieve_mongodb.py

--- retrieve_mongodb.py
+++ retrieve_mongodb.py
@@ -1,13 +1,11 @@
 from pymongo import MongoClient
 
-# Connect to MongoDB Atlas
+# Connecting to MongoDB Atlas
 mongo_client = MongoClient("mongodb+srv://ameereking:bJx3Tov81KBpaeJ2@cluster0.jeaqbgq.mongodb.net/OrininuStreamingDB?retryWrites=true&w=majority&appName=Cluster0")
 mongo_db = mongo_client["OrininuStreamingDB"]
 
-# Define collections to retrieve data from
 collections = ["artists", "albums", "users"]
 
-# Retrieve and display data from each collection
 for collection_name in collections:
     mongo_collection = mongo_db[collection_name]
     documents = mongo_collection.find()
@@ -16,4 +14,3 @@ for collection_name in collections:
     for doc in documents:
         print(doc)
 
-print("\n🔥 Data retrieval from MongoDB complete!")
